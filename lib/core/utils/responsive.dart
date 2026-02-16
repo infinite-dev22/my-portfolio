39106e8f5182833fcc92b6f5503ab1811ce8b7f1
infinite-dev22 <mwigojm@gmail.com>
2026-02-16 23:35:38 +0300

    Refactor responsive breakpoints
    Update the screen size breakpoints for mobile, tablet, and desktop layouts. The layout builder logic is also adjusted to match the new breakpoint values.

--- lib/core/utils/responsive.dart
+++ lib/core/utils/responsive.dart
@@ -13,25 +13,26 @@ class ResponsiveLayout extends StatelessWidget {
   });
 
   static bool isMobile(BuildContext context) =>
-      MediaQuery.of(context).size.width < 850;
+      MediaQuery.of(context).size.width < 420;
 
   static bool isTablet(BuildContext context) =>
-      MediaQuery.of(context).size.width >= 850 &&
-      MediaQuery.of(context).size.width < 1100;
+      MediaQuery.of(context).size.width < 1290 &&
+      MediaQuery.of(context).size.width > 420;
 
   static bool isDesktop(BuildContext context) =>
-      MediaQuery.of(context).size.width >= 1100;
+      MediaQuery.of(context).size.width >= 1290;
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
-        if (constraints.maxWidth >= 1100) {
-          return desktop;
-        } else if (constraints.maxWidth >= 850) {
+        if (constraints.maxWidth <= 450) {
+          return mobile;
+        }
+        if (constraints.maxWidth > 450 && constraints.maxWidth < 1000) {
           return tablet ?? mobile;
         } else {
-          return mobile;
+          return desktop;
         }
       },
     );
